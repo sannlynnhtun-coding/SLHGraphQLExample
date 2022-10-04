1f7540e82109dedabd956244db817f4847d64040
Sann Lynn Htun <sannlynnhtun.error@gmail.com>
2022-10-04 18:27:53 +0630

    Add project files.

---
+++ GraphQL20221004/EfDbContext/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using GraphQL20221004.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace GraphQL20221004.EfDbContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var configuration = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json")
+                   .Build();
+
+                var connectionString = configuration.GetConnectionString("DbConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
---
+++ GraphQL20221004/Startup.cs
@@ -0,0 +1,60 @@
+using GraphQL20221004.EfDbContext;
+using GraphQL20221004.Queries;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQL20221004
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseSqlServer(Configuration.GetConnectionString("DbConnection"));
+            });
+            services.AddGraphQLServer().AddQueryType<BlogQuery>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapGraphQL();
+            });
+        }
+    }
+}
